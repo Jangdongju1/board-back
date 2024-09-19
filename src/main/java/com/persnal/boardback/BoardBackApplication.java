ad8c019492bfb5badbabd125086cde1e159c209a
jang <jdj81204@naver.com>
2024-09-20 00:58:27 +0900

    주석추가 테스트

--- src/main/java/com/persnal/boardback/BoardBackApplication.java
+++ src/main/java/com/persnal/boardback/BoardBackApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(exclude = {})
 public class BoardBackApplication {
 	public static void main(String[] args) {
+		//주석추가
 		SpringApplication.run(BoardBackApplication.class, args);
 
 	}
